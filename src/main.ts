aacec61b3158bc1d017d7588ab073d499d797e61
Jignasu Pathak <63716051+Mister-JP@users.noreply.github.com>
2025-05-07 00:32:41 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,6 @@ import App from './App.vue'
 import '@vue-flow/core/dist/style.css'
 import '@vue-flow/core/dist/theme-default.css'
 
-// --- UPDATE THIS LINE ---
-import '@vue-flow/background/actual-background-style.css' // Or whatever path you found
 
 import '@vue-flow/minimap/dist/style.css'     // Assuming this one is correct
 import '@vue-flow/controls/dist/style.css'    // Assuming this one is correct
